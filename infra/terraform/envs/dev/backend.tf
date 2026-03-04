82ccc8edd9f32a05c17fee91e5302928fd113ff3
Himanshuo3 <Himanshu03desai@gmail.com>
2026-03-04 17:00:29 -0500

    Update backend

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "orionops-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "orionops-terraform-locks"
-    encrypt        = true
-  }
-}
+
